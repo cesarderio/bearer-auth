f875805c150227358361ad18d343a0291944b4d5
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-12-06 16:14:26 -0800

    stuck on basic auth test

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../models/index.js');
+
+
+
+module.exports = async (req, res, next) => {
+
+  try {
+    if (!req.headers.authorization) {
+      res.status(401).send('Not Authorized!');
+    }
+
+    let basic = req.headers.authorization;
+    let authString = basic.split(' ').pop();
+
+
+
+    let [username, pass] = base64.decode(authString).split(':');
+
+    req.user = await users.authenticateBasic(username, pass);
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(403).send('Invalid Login');
+  }
+
+};
